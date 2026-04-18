ee59bf0978d606141856e67cc1a486ecaf5e5f1c
Marcos <57899906+marcosmjr@users.noreply.github.com>
2026-04-18 16:01:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function enviarDados(){
+    let inpuEmail = document.getElementById('email');
+    let inputSenha = document.getElementById('senha');
+
+    alert(inpuEmail.value + " " + inputSenha.value)
+}
